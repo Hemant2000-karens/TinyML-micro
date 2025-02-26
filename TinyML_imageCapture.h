5e05783fa8a8ef35bb47c397dff16b51bb215c83
Hemant <team.karensbook@icloud.com>
2025-02-26 12:58:46 +0530

    added Camera Setup file.

---
+++ TinyML_imageCapture.h
@@ -0,0 +1,14 @@
+#ifndef TinyML_imageCapture
+#define TinyML_imageCapture
+
+#include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+
+TfLiteStatus GetImage(
+  tflite::ErrorReporter* error_reporter, 
+  int image_width,
+  int image_height,
+  int channels,
+  int8_t* image_data);
+
+#endif
